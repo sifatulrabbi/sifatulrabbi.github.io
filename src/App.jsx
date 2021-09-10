34c97b2d262882fbdb90eb6ae15bb523fa906146
temujins <mdsifatulislam.rabbi@gmail.com>
2021-09-10 19:56:52 +0600

    adding variables and mixins

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scss/App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
