0c65fb16d91b702cbb05f3732b6845c29baa45d5
temujins <mdsifatulislam.rabbi@gmail.com>
2021-09-11 10:33:35 +0600

    resolved #1 : added navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { logo } from '../../images';
 
 const Navbar = () => {
+  const [shadow, setShadow] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = (window.onscroll = () => {
+      document.documentElement.scrollTop > 50 ? setShadow(true) : setShadow(false);
+    });
+
+    return () => unsubscribe;
+  }, []);
+
   return (
-    <div className="navbar">
-      <div className="logo">
+    <div className={`navbar ${shadow && 'showShadow'}`}>
+      <div className="navbar_logo">
         <img src={logo} alt="Sifatul" />
       </div>
       <nav>
         <ul className="navbar_linksList">
-          <li className="navbar_linkList_item">Home</li>
-          <li className="navbar_linkList_item">Projects</li>
-          <li className="navbar_linkList_item">Contact</li>
-          <li className="navbar_linkList_item">About</li>
+          <li className="navbar_linksList_item">Home</li>
+          <li className="navbar_linksList_item">Projects</li>
+          <li className="navbar_linksList_item">Contact</li>
+          <li className="navbar_linksList_item">About</li>
         </ul>
       </nav>
     </div>
