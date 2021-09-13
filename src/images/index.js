43c5e888427ebbee958807b552e26c4463ef129b
temujins <mdsifatulislam.rabbi@gmail.com>
2021-09-13 10:56:05 +0600

    resolved #12 : bottom bar added

--- src/images/index.js
+++ src/images/index.js
@@ -1,7 +1,3 @@
 export { default as logo } from './s-logo-nobg.png';
 export { default as logoWhite } from './s-logo-nobg-white.png';
 export { default as bgIllustration } from './bg-illustration.svg';
-export { default as homeIcon } from './home-icon.png';
-export { default as workIcon } from './work-icon.png';
-export { default as aboutIcon } from './about-icon.png';
-export { default as messageIcon } from './message-icon.png';
