1cd4ddfc045d539668f643af80ab461b716b0caa
temujins <mdsifatulislam.rabbi@gmail.com>
2021-09-13 14:12:14 +0600

    resolved #6 : reusable project card component

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Title from '../Title/Title';
+import ProjectCard from '../ProjectCard/ProjectCard';
+import data from './data';
 
 const ProjectSection = () => {
   return (
     <div className="projectSection">
       <Title title="My Projects" />
+      <div className="projectSection_wrapper">
+        {data.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            label={project.label}
+            desc={project.desc}
+            img={project.img}
+            links={project.links}
+          />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/ProjectSection/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    id: 'myPortfolioProject002301',
+    title: 'My Portfolio website',
+    desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    img: '',
+    links: {
+      github: 'https://github.com/temujins/temujins.github.io',
+      live: 'https://temujins.github.io',
+    },
+  },
+  {
+    id: 'myPortfolioProject12455',
+    title: 'My Portfolio website',
+    desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    img: '',
+    links: {
+      github: 'https://github.com/temujins/temujins.github.io',
+      live: 'https://temujins.github.io',
+    },
+  },
+  {
+    id: 'myPortfolioProject275914',
+    title: 'My Portfolio website',
+    desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    img: '',
+    links: {
+      github: 'https://github.com/temujins/temujins.github.io',
+      live: 'https://temujins.github.io',
+    },
+  },
+  {
+    id: 'myPortfolioProject083579',
+    title: 'My Portfolio website',
+    desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    img: '',
+    links: {
+      github: 'https://github.com/temujins/temujins.github.io',
+      live: 'https://temujins.github.io',
+    },
+  },
+];
+
+export default data;
