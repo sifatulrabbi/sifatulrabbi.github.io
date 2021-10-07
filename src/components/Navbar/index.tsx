16fc7afd25737a850d09e5a2bfe9ef42300d53ab
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-07 15:48:19 +0600

    state setter name changed
    - showSet() in now setShow()

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,18 +3,18 @@ import { Wrapper, MenuIcon } from './navbar.styles';
 import { logo } from '../../images';
 
 interface Props {
-  showSet: () => void;
+  setShow: () => void;
   show: boolean;
 }
 
 class Navbar extends React.Component<Props> {
   render() {
-    const { showSet, show } = this.props;
+    const { setShow, show } = this.props;
 
     return (
       <Wrapper>
         <img className='navbar_logo' src={logo} alt='logo' />
-        <button className='navbar_button' onClick={showSet}>
+        <button className='navbar_button' onClick={setShow}>
           <MenuIcon show={show}>
             <span className='bar top' />
             <span className='bar center' />
