64219bc593edb526e440c13f367246a348433665
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-10 12:02:25 +0600

    adding about section

---
+++ src/components/About/About.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Wrapper } from './about.styles';
+
+interface Props {}
+interface State {}
+
+class About extends Component<Props, State> {
+  state = {};
+
+  render() {
+    return <Wrapper></Wrapper>;
+  }
+}
+
+export default About;
