be40f78391074406a46f6898038aed3e17a0a42b
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-10 12:01:29 +0600

    added hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Wrapper } from './hero.styles';
+import { H1, Caption, Body1 } from '../Typography';
+import Button from '../Button';
+
+interface Props {}
+interface State {}
+
+class Hero extends Component<Props, State> {
+  render() {
+    return (
+      <Wrapper>
+        <Caption big gutterBottom>
+          Hello, my name is
+        </Caption>
+        <H1>Sifatul Rabbi</H1>
+        <H1 secondary>I do web development</H1>
+        <Body1 secondary classes='hero-desc'>
+          I'm a self-taught and passionate web developer from Bangladesh.
+          Currently working as a freelancer. I'm focused on the front-end and
+          creating the best and smoothest UI for websites.
+        </Body1>
+        <Button label='See my works' big callBack={(e) => e} />
+      </Wrapper>
+    );
+  }
+}
+
+export default Hero;
---
+++ src/components/Hero/hero.styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  width: 100%;
+  height: max-content;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  grid-gap: 1rem;
+
+  .hero-desc {
+    margin-bottom: 4rem;
+    width: 100%;
+    max-width: 60ch;
+  }
+`;
