39c7abc2e8d3351e8ec64de7486119ff02984de8
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-11 13:28:27 +0600

    New section: about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { Wrapper } from './about.styles';
+import SectionTitle from '../SectionTitle/SectionTitle';
+import { Body1 } from '../Typography';
 
 interface Props {}
 interface State {}
 
 class About extends Component<Props, State> {
-  state = {};
-
   render() {
-    return <Wrapper></Wrapper>;
+    return (
+      <Wrapper>
+        <SectionTitle label={'About Me'} index={1} />
+        <article className='grid'>
+          <Body1 classes='about-me' secondary>
+            Hello! My name is Brittany and I enjoy creating things that live on
+            the internet. My interest in web development started back in 2012
+            when I decided to try editing custom Tumblr themes — turns out
+            hacking together a custom reblog button taught me a lot about HTML &
+            CSS!
+            <br />
+            <br />
+            Fast-forward to today, and I’ve had the privilege of working at an
+            advertising agency, a start-up, a huge corporation, and a
+            student-led design studio. My main focus these days is building
+            accessible, inclusive products and digital experiences at
+            Upstatement for a variety of clients.
+            <br />
+            <br />
+            I also recently launched a course that covers everything you need to
+            build a web app with the Spotify API using Node & React.
+            <br />
+            <br />
+            Here are a few technologies I’ve been working with recently:
+          </Body1>
+          <div className='imgWrapper'></div>
+        </article>
+      </Wrapper>
+    );
   }
 }
 
--- src/components/About/about.styles.ts
+++ src/components/About/about.styles.ts
@@ -1,3 +1,65 @@
 import styled from 'styled-components';
+import { myImg } from '../../images';
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  width: max-content;
+  max-width: 100%;
+  height: max-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr;
+    align-items: flex-start;
+    grid-gap: 5rem;
+  }
+
+  .about-me {
+    width: 100%;
+    max-width: 57ch;
+  }
+
+  .imgWrapper {
+    justify-self: center;
+    position: relative;
+    height: clamp(180px, 20vw, 300px);
+    width: clamp(180px, 20vw, 300px);
+    border-radius: ${({ theme }) => theme.borderRadius};
+    background-image: url(${myImg});
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: soft-light;
+    background-color: ${({ theme }) => theme.palette.transparent};
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      top: 0;
+      left: 0;
+      border-radius: ${({ theme }) => theme.borderRadius};
+      border: 2px solid ${({ theme }) => theme.palette.main};
+      transform: translate(10%, 10%);
+      z-index: -1;
+      transition: ${({ theme }) => theme.transition('transform')};
+    }
+
+    &:hover {
+      background-blend-mode: normal;
+
+      &::before {
+        transform: translate(5%, 5%);
+      }
+    }
+  }
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint}) {
+    .grid {
+      grid-template-columns: auto 1fr;
+    }
+  }
+`;
