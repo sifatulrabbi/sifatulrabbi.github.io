85a3d4b14c0fc1154b01c4a9b5e2893476287cb8
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-11 13:27:54 +0600

    updated index.tsx

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -2,3 +2,4 @@ export { default as H1 } from './H1';
 export { default as Caption } from './Caption';
 export { default as Body1 } from './Body1';
 export { default as Body2 } from './Body2';
+export { default as H3 } from './H3';
