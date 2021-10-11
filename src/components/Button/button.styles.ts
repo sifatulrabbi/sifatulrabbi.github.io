d10fca5ad8ec0a0c003d19253821bf336aa2300b
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-11 08:09:15 +0600

    reduced padding for big button

--- src/components/Button/button.styles.ts
+++ src/components/Button/button.styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.button<{ big?: true }>`
-  padding: ${({ big }) => (big ? `1.2rem 2.8rem` : `1rem 1.5rem`)};
+  padding: ${({ big }) => (big ? `1rem 2.8rem` : `.8rem 1.2rem`)};
   border: ${({ theme }) => `2px solid ${theme.palette.main}`};
   border-radius: ${({ theme }) => theme.borderRadius};
   transition: ${({ theme }) => theme.transition('background-color')};
