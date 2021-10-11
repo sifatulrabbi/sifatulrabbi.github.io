70507ebb62df84fc2c37feaaf427947089afceea
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-11 13:27:14 +0600

    added blur effect in the navbar

--- src/components/Navbar/navbar.styles.ts
+++ src/components/Navbar/navbar.styles.ts
@@ -16,11 +16,14 @@ export const Wrapper = styled.nav<{ show: boolean; shrink: boolean }>`
   padding: 0 1.5rem;
   transform: translateY(${({ show }) => (show ? '0%' : '-100%')});
   transition: ${({ theme }) => theme.transition()};
+  backdrop-filter: blur(10px);
 
   ${({ shrink, theme }) =>
     shrink
-      ? `height: 70px; box-shadow: ${theme.shadow[1]};`
-      : `  height: 100px;`}
+      ? `height: 70px; box-shadow: ${theme.shadow[1]};
+        background-color: ${theme.palette.transDark};`
+      : `height: 100px;
+        background-color: ${theme.palette.dark};`}
 
   .navbar-logo {
     height: 50px;
@@ -29,49 +32,5 @@ export const Wrapper = styled.nav<{ show: boolean; shrink: boolean }>`
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoint}) {
     padding: 0 3vw;
-  }
-`;
-
-export const MenuButton = styled.button<{ active: boolean }>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 35px;
-  width: 35px;
-  position: relative;
-
-  .bar {
-    display: block;
-    height: 3px;
-    background-color: ${({ theme }) => theme.palette.main};
-    border-radius: ${({ theme }) => theme.borderRadius};
-    transition: ${({ theme }) => theme.transition()};
-    position: absolute;
-    z-index: 1500;
-    top: 50%;
-    right: ${({ active }) => (active ? `50%` : `0`)};
-
-    &.small {
-      ${({ active }) =>
-        active
-          ? `width: 100%;
-            transform: translate(50%, -50%) rotate(-45deg);`
-          : `width: 80%;
-            transform: translate(0, 150%);`};
-    }
-
-    &.big {
-      ${({ active }) =>
-        active
-          ? `width: 100%;
-            transform: translate(50%, -50%) rotate(45deg);`
-          : `width: 100%;
-            transform: translate(0, -300%);`};
-    }
-  }
-
-  @media screen and (min-width: ${({ theme }) => theme.breakpoint}) {
-    display: none;
   }
 `;
