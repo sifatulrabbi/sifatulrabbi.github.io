0f0794916f5ffc6aca0ec264e25ff6549e704490
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-12 17:17:18 +0600

    added works data to each component

--- src/components/Work/data.ts
+++ src/components/Work/data.ts
@@ -2,6 +2,7 @@ import { FiGithub, FiExternalLink } from 'react-icons/fi';
 import { IconType } from 'react-icons';
 
 export type WorksData = {
+  id: string;
   name: string;
   desc: string;
   tags: string[];
@@ -9,12 +10,13 @@ export type WorksData = {
     icon: IconType;
     link: string;
   }[];
-}[];
+};
 
-const data = [
+const data: WorksData[] = [
   {
+    id: 'asdfghjklqwertv',
     name: 'Arkanoid Game',
-    desc: 'description',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
     tags: ['TypeScript', 'CSS', 'HTML'],
     links: [
       { icon: FiGithub, link: 'hello' },
@@ -22,8 +24,19 @@ const data = [
     ],
   },
   {
+    id: 'asdfghjklqwertx',
     name: 'Social media app',
-    desc: 'description',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
+    tags: ['React', 'JavaScript', 'styled components', 'firebase'],
+    links: [
+      { icon: FiGithub, link: 'hello' },
+      { icon: FiExternalLink, link: 'hello' },
+    ],
+  },
+  {
+    id: 'asdfghjklqwertn',
+    name: 'Full stack blog',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
     tags: ['React', 'JavaScript', 'styled components', 'firebase'],
     links: [
       { icon: FiGithub, link: 'hello' },
