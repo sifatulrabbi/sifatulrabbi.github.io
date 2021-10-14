58387579e7b5abbfe7db05e51acb982d26ed5c59
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-14 23:12:31 +0600

    added new work data

--- src/components/Work/data.ts
+++ src/components/Work/data.ts
@@ -1,52 +1,81 @@
-import { FiGithub, FiExternalLink } from 'react-icons/fi';
-import { IconType } from 'react-icons';
 import { testImg } from '../../images';
 
 export type WorksData = {
-  id: string;
   name: string;
   desc: string;
   tags: string[];
   img: string;
   links: {
-    icon: IconType;
-    link: string;
-  }[];
+    github: string;
+    external: string;
+  };
 };
 
 const data: WorksData[] = [
   {
-    id: 'asdfghjklqwertv',
     name: 'Arkanoid Game',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
     tags: ['TypeScript', 'CSS', 'HTML'],
     img: testImg,
-    links: [
-      { icon: FiGithub, link: 'hello' },
-      { icon: FiExternalLink, link: 'hello' },
-    ],
+    links: {
+      github: 'https://github.com/temujins/ts-arkanoid-game',
+      external: 'https://temujins.github.io/ts-arkanoid-game/',
+    },
   },
+
   {
-    id: 'asdfghjklqwertx',
-    name: 'Social media app',
+    name: 'Full Stack blog',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
-    tags: ['React', 'JavaScript', 'styled components', 'firebase'],
+    tags: ['React', 'JavaScript', 'Styled Components', 'MongoDB'],
     img: testImg,
-    links: [
-      { icon: FiGithub, link: 'hello' },
-      { icon: FiExternalLink, link: 'hello' },
-    ],
+    links: {
+      github: 'https://github.com/temujins/full-stack-blog-app',
+      external: '',
+    },
   },
+
   {
-    id: 'asdfghjklqwertn',
-    name: 'Full stack blog',
+    name: 'Social Media website',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
-    tags: ['React', 'JavaScript', 'styled components', 'firebase'],
+    tags: ['React', 'JavaScript', 'Styled Components', 'Firebase'],
     img: testImg,
-    links: [
-      { icon: FiGithub, link: 'hello' },
-      { icon: FiExternalLink, link: 'hello' },
-    ],
+    links: {
+      github: 'https://github.com/temujins/social_media_app',
+      external: 'https://socialize-reactapp.web.app/',
+    },
+  },
+
+  {
+    name: 'My Portfolio website',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
+    tags: ['TypeScript', 'React', 'Styled Components'],
+    img: testImg,
+    links: {
+      github: 'https://github.com/temujins/temujins.github.io',
+      external: '',
+    },
+  },
+
+  {
+    name: 'Todo app',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
+    tags: ['TypeScript', 'React', 'Styled Components'],
+    img: testImg,
+    links: {
+      github: 'https://github.com/temujins/todo-app-fm',
+      external: 'https://react-ts-todo-app.vercel.app/',
+    },
+  },
+
+  {
+    name: 'Simple Snake Game',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt exercitationem provident non dolorum maxime ex suscipit voluptas, reprehenderit dignissimos natus.',
+    tags: ['JavaScript', 'HTML', 'CSS'],
+    img: testImg,
+    links: {
+      github: 'https://github.com/temujins/snake-game',
+      external: '',
+    },
   },
 ];
 
