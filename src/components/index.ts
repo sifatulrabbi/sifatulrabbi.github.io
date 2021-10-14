9a1f6e61aa31018325d326bd53175937a7fd4140
temujins <mdsifatulislam.rabbi@gmail.com>
2021-10-14 09:29:38 +0600

    adds footer section

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Hero } from './Hero/Hero';
 export { default as Work } from './Work/Work';
 export { default as About } from './About/About';
 export { default as Contact } from './Contact/Contact';
+export { default as Footer } from './Footer/Footer';
