cde605d8e634fea985566e4e0e15cb4efae48947
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-03-27 14:08:16 +0600

    feat: projects section

--- components/my-projects/cards.tsx
+++ components/my-projects/cards.tsx
@@ -5,9 +5,9 @@ import {v4} from "uuid";
 
 export const Cards: React.FC = () => {
     return (
-        <div className="mt-[8vh] flex flex-col gap-8 justify-start items-center">
-            {portfolioData.projects.map((data, index) => (
-                <Card key={v4()} {...data} reverse={index % 2 !== 0} />
+        <div className="mt-[8vh] flex flex-col justify-start items-start mx-auto gap-12">
+            {portfolioData.projects.map((data) => (
+                <Card key={v4()} {...data} />
             ))}
         </div>
     );
