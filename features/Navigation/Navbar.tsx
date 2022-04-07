22a810b1152648339ca3ad56b688e294d0e8a227
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-04-07 12:00:37 +0600

    fix: increase z-index of navbar component

--- features/Navigation/Navbar.tsx
+++ features/Navigation/Navbar.tsx
@@ -4,7 +4,7 @@ import {Logo} from "../../components";
 export const Navbar: React.FC = () => {
     return (
         <header
-            className="fixed top-0 left-0 right-0 bg-white bg-opacity-95 px-4 flex justify-between items-center h-[60px]"
+            className="z-[50] fixed top-0 left-0 right-0 bg-white bg-opacity-95 px-4 flex justify-between items-center h-[60px]"
             style={{borderTop: "5px solid #0077ff"}}
         >
             <div className="w-container w-full">
