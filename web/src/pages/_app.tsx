8c16438f72a34c3e934ee04ad2b9b7c5ca40e7f7
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-09-30 13:04:44 +0600

    new dir structure

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,19 +1,13 @@
-import "../styles/globals.scss"
+import "../../styles/global.scss"
 import {Suspense} from "react"
 import type {AppProps} from "next/app"
-import {Navbar, MobileBar} from "../features"
-import {Footer} from "../components"
 import {RecoilRoot} from "recoil"
-import {LoadingScreen} from "../features/LoadingScreen"
 
 function MyApp({Component, pageProps}: AppProps) {
 	return (
 		<RecoilRoot>
-			<Suspense fallback={<LoadingScreen />}>
-				<Navbar />
-				<MobileBar />
+			<Suspense fallback={<div>Loading...</div>}>
 				<Component {...pageProps} />
-				<Footer />
 			</Suspense>
 		</RecoilRoot>
 	)
---
+++ web/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	content: ["./src/**/*.{tsx,ts,js,jsx}"],
+	darkMode: "class",
+	theme: {
+		fontFamily: {
+			primary: ["Plus Jakarta Sans", "sans-serif"],
+			monospace: ["Fira Code", "monospace"],
+		},
+		extend: {
+			colors: {
+				primary: "#0070ff",
+				secondary: "#576f8e",
+			},
+		},
+	},
+	plugins: [],
+}
