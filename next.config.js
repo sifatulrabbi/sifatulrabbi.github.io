c75a41ed9c342c259333be6aad87ce1afdd3bd28
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2023-01-28 23:13:07 +0600

    deploy on cpanel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    images: {
-        loader: "akamai", // adding image loader to prevent next export error
-        path: "",
-    },
 };
 
 module.exports = nextConfig;
