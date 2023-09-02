7e3970613e3baf2617e22d631cb7cc0a84da01d7
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2023-09-02 13:13:50 +0600

    update for cpanel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // images: {
+    //     loader: "imgix",
+    // },
 };
 
 module.exports = nextConfig;
