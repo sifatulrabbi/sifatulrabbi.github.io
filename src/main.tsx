ad2d6ef17984025bbf81b91b3f88f18c04970b4c
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2023-10-26 14:49:58 +0600

    create new version for the portfolio

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+
+import "./styles/index.scss";
+
+const renderElement = document.getElementById("root");
+const root = ReactDOM.createRoot(renderElement!);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
