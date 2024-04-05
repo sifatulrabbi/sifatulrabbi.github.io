13fdca79dd14776f0c92dceee4a0b94efd591b5c
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-04-05 10:37:52 +0600

    update experience section

--- src/modules/DetailsSection/ExperiencesSection.tsx
+++ src/modules/DetailsSection/ExperiencesSection.tsx
@@ -7,51 +7,82 @@ const experienceData = [
         companyName: "HelloScribe A.I",
         companyLocation: "Florida, USA",
         companyUrl: "https://helloscribe.ai",
-        position: "Full Stack Engineer",
-        from: "Nov 2022",
-        till: "Present",
-        current: true,
-        jobType: "Remote",
-        employmentType: "Contract",
-        description: [
-            "Gained 120% more users and 40% more monthly active users within 2 months by leading the development process of HelloScribe AI v2.",
-            "Ensured 100% login success and resolved security issues by adding Auth0 and refactoring the authentication flow, which also ensured a smooth transition for the existing users.",
-            "Improved the app's performance by an average of 300% through careful refactoring, rewriting, and restructuring.",
-            "Enhanced team efficiency by reorganizing and optimizing the code base, maintaining clear and effective communication, and developing scripts for testing, identifying, and automating the debugging process.",
-        ],
-        techStack: [
-            "JavaScript",
-            "Python",
-            "Node.js",
-            "Express.js",
-            "Vue.js",
-            "MongoDB",
+        positions: [
+            {
+                position: "Co-Founder & Lead Software Developer",
+                from: "Feb 2024",
+                till: "Present",
+                current: true,
+                jobType: "Remote",
+                employmentType: "Contract",
+                description: [
+                    "Focused on building and designing innovative features by leveraging AI technologies to simplify and enhance everyday life.",
+                    "Leading the development process and the team to implement new features, architect the system, and improve the quality of the services.",
+                ],
+                techStack: [
+                    "Python",
+                    "JavaScript",
+                    "Node.js",
+                    "Go",
+                    "Express.js",
+                    "Vue.js",
+                    "MongoDB",
+                ],
+            },
+            {
+                position: "Full stack Developer",
+                from: "Nov 2022",
+                till: "Feb 2024",
+                current: false,
+                jobType: "Remote",
+                employmentType: "Contract",
+                description: [
+                    "Leading the development process and the team to implement new features, architect the system, and improve the quality of the services.",
+                    "3x the system performance by restructuring the apps to utilize Async and concurrency, refactoring the frontend app to render effectively, and implementing various principles and best practices across the system.",
+                    "Streamlined the development process of HelloScribe AI v2 and ensured a 100% login success rate which gained 120% more users and 40% more monthly active users.",
+                    "Playing a key role in building and designing a scalable and robust system.",
+                ],
+                techStack: [
+                    "Python",
+                    "JavaScript",
+                    "Node.js",
+                    "Go",
+                    "Express.js",
+                    "Vue.js",
+                    "MongoDB",
+                ],
+            },
         ],
     },
     {
         companyName: "X-Booker",
         companyLocation: "Chessington, UK",
         companyUrl: "https://x-booker.co.uk",
-        position: "Full Stack Engineer",
-        from: "Apr 2022",
-        till: "Present",
-        current: true,
-        jobType: "Remote",
-        employmentType: "Contract",
-        description: [
-            "<strong>Led</strong> the development process of the X-Booker SaaS platform by designing and developing multiple backend and frontend applications, ensuring security, and scalability.",
-            "<strong>Mentored and assisted</strong> junior developers in understanding the platform, developing new features, and ensuring a smooth onboarding process.",
-            "Built a reliable <strong>CI/CD pipeline</strong> using Github Actions, Docker, and Kubernetes, resulting in a faster and more dependable deployment process.",
-        ],
-        techStack: [
-            "TypeScript",
-            "Python",
-            "Node.js",
-            "Next.js",
-            "GCP",
-            "Docker",
-            "Redis",
-            "Kubernetes",
+        positions: [
+            {
+                position: "Lead Software Developer",
+                from: "Apr 2022",
+                till: "Feb 2024",
+                current: false,
+                jobType: "Remote",
+                employmentType: "Contract",
+                description: [
+                    "Leading the development process and team to design and build the X-Booker system to establish a firm foundation for the SaaS.",
+                    "Mentored and assisted freshers in understanding the platform, developing new features, and ensuring a smooth onboarding process.",
+                    "Utilizing in-depth software development skills to build various automation as well as integrate different APIs and services with X-Booker.",
+                    "Built a reliable CI/CD pipeline using Github Actions, Docker, and Kubernetes, resulting in a faster and more dependable deployment process.",
+                ],
+                techStack: [
+                    "TypeScript",
+                    "Python",
+                    "Node.js",
+                    "Next.js",
+                    "GCP",
+                    "Docker",
+                    "Redis",
+                    "Kubernetes",
+                ],
+            },
         ],
     },
 ];
@@ -80,13 +111,15 @@ const ExperiencesSection: React.FC = () => {
             {experienceData.map((d) => (
                 <div
                     key={d.companyName}
-                    className="w-full prose max-w-full prose-h4:text-slate-300 prose-h4:font-medium prose-h4:my-2 text-slate-400 prose-strong:text-slate-400"
+                    className="w-full prose max-w-full prose-h4:text-slate-300 prose-h4:font-medium prose-h4:my-2 text-slate-400 prose-strong:text-slate-400 flex flex-col border-b-2 border-slate-700 last:border-transparent mb-8"
                 >
-                    <small className="flex flex-row items-center gap-1 text-slate-500 font-mono">
-                        {d.from} <FaMinus className="text-xs" /> {d.till}
-                    </small>
-                    <a href={d.companyUrl} target="_blank" rel="noreferrer">
-                        <h4 className="flex items-center gap-1">
+                    <a
+                        href={d.companyUrl}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="hover:underline"
+                    >
+                        <h4 className="flex items-center gap-1 hover:underline">
                             {d.companyName}
                             <div className="rounded-full bg-slate-300 w-1 h-1" />
                             <span className="font-normal text-sm">
@@ -95,27 +128,48 @@ const ExperiencesSection: React.FC = () => {
                             <FaArrowRight className="text-xs -rotate-45" />
                         </h4>
                     </a>
-                    <div className="text-primary-400">{d.position}</div>
-                    <ul className="text-slate-400">
-                        {d.description.map((desc) => (
-                            <li
-                                key={desc}
-                                dangerouslySetInnerHTML={{ __html: desc }}
-                            />
-                        ))}
-                    </ul>
-                    <div className="w-full flex flex-row items-start justify-start flex-wrap gap-2">
-                        {d.techStack.map((t) => (
-                            <span
-                                key={`tech-stack-${t}`}
-                                className="inline-block text-xs px-3 py-1 rounded-full bg-slate-800"
+                    {d.positions.map((p) => (
+                        <div
+                            key={p.position + p.from + p.till}
+                            className="flex flex-col"
+                        >
+                            <small className="flex flex-row items-center gap-1 text-slate-500 font-mono">
+                                {p.from} <FaMinus className="text-xs" />{" "}
+                                {p.till}
+                            </small>
+                            <div
+                                className={`text-primary-400
+                                ${
+                                    p.current
+                                        ? "text-primary-200 font-bold"
+                                        : ""
+                                }`}
                             >
-                                {t}
-                            </span>
-                        ))}
-                    </div>
-                    <br />
-                    <br />
+                                {p.position}
+                            </div>
+                            <ul className="text-slate-400 mt-0">
+                                {p.description.map((desc) => (
+                                    <li
+                                        key={desc}
+                                        dangerouslySetInnerHTML={{
+                                            __html: desc,
+                                        }}
+                                    />
+                                ))}
+                            </ul>
+                            <div className="w-full flex flex-row items-start justify-start flex-wrap gap-2">
+                                {p.techStack.map((t) => (
+                                    <span
+                                        key={`tech-stack-${t}`}
+                                        className="inline-block text-xs px-3 py-1 rounded-full bg-slate-800"
+                                    >
+                                        {t}
+                                    </span>
+                                ))}
+                            </div>
+                            <br />
+                        </div>
+                    ))}
                 </div>
             ))}
         </section>
