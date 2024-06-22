aecfa5e3ffe4349467a19b5a7489859a85308294
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-06-22 08:21:06 +0600

    update experience info

--- src/modules/TerminalMode/experienceData.ts
+++ src/modules/TerminalMode/experienceData.ts
@@ -5,11 +5,15 @@ export const experiencesData = [
         text: `**Co-Founder & Lead Software Engineer**  
 HelloScribe AI, Florida, USA -- Full time, Remote -- Nov, 2022 - Present
 
-- Led the newly formed remote team as the company transitioned from MVP to the production phase and acquired 3000+ new users
-- Built a metered billing system in 7 weeks that made subscriptions more flexible and 50% cheaper
-- Reduced the login/registration defect rate to 0% and improved the security by rewriting all the Auth modules in 2 weeks
-- Architected a performant and flexible data processing pipelines that can handle up to 1,000 concurrent requests by utilizing Python’s async and concurrency
-- Improved debugging velocity by implementing detailed logging in the production apps
+*HelloScribe AI is an AI productivity tool that speeds up various planning and content-writing tasks. It provides an intuitive web app and utilizes LLM engines to aid in plan-making and content writing.*
+
+- Collaborated with the founders to develop better and more time-effective solutions to improve and evolve the system
+- Developed a flexible algorithm for the HelloScribe agent, laid the groundwork to implement a dynamic UI component generator from the core LLM, and completed the dynamic HTML table generator
+- Improved the core data pipeline of HelloScribe, enhancing performance from handling a few dozen concurrent requests to managing up to 5,000 concurrent requests
+- Architected a flexible pay-as-you-go billing system
+- Improved the data models and frontend UI structure for better performance and scalability
+- Led the development process to rebuild HelloScribe AI with massive improvement and new features such as the agent mode, better text editor, improved security, and scalable backend architecture
+- Enforced coding styles and consistencies across team members
 
 `,
     },
@@ -19,10 +23,17 @@ HelloScribe AI, Florida, USA -- Full time, Remote -- Nov, 2022 - Present
         text: `**Lead Full-stack Engineer**  
 X-Booker, Chessington, UK -- Contract, Remote -- Apr, 2022 - Dec, 2023
 
-- Mentored and assisted junior developers, in understanding the platform, and developing new features
-- Architected a Tired per-user subscription system with buyable perks using Stripe
-- Developed a pub-sub solution using Node.js (TypeScript) and Firestore that performs a set of automated tasks in the background while keeping the main thread open to serve clients
-- Built a secured API key-based third-party integration for the X-Booker API
-- Built a CI/CD pipeline to auto-deploy two versions (Production & Staging) of the system on K8s`,
+*X-Booker aims to streamline project management by combining most of the project management solutions into one single system with intelligent automation as well as providing an intuitive UI for easier navigation and business management.*
+
+- As the lead engineer, I worked closely with the founders to figure out technical solutions and the best way to materialize their vision
+- Integrated various services such as Google Maps, Gmail, and Calendar into X-Booker's core API
+- Architected a diverse system that uses background job processing, pub-sub, and a serverless approach to achieve various automation and data processing
+- Built a flexible chat system with the ability to chat with individuals create groups, and share images, videos, and audio
+- Architected a tier-based subscription system with buyable perks (extra storage or extra team seat)
+- Built a complex but responsive, fast-loading, and secured dashboard, kanban board, email client, and file manager with different access levels (super admin, admin, and different types of teams)
+- Architected a role-based auth system as well as an API integration so that the organizations can have direct external connections with other lead generation platforms
+- Onboarded and mentored junior developers, in understanding the platform, and developing new features
+- Developed an automated deployment pipeline to auto-deploy two different versions of the system on GCP’s Kubernetes cluster
+`,
     },
 ];
